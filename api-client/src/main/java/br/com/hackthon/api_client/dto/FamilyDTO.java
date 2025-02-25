bf0051a95f0eb6ad06c007e622478c47e2ea58eb
igorgcf <igorTecnologia@outlook.com>
2025-02-25 20:32:58 -0300

    Family DTO

--- api-client/src/main/java/br/com/hackthon/api_client/dto/FamilyDTO.java
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/FamilyDTO.java
@@ -85,4 +85,17 @@ public class FamilyDTO extends RepresentationModel<FamilyDTO>  {
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        FamilyDTO familyDTO = (FamilyDTO) o;
+        return Objects.equals(id, familyDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
