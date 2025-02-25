521d5c5422096da2efa3361d667a32d7afa9da2b
igorgcf <igorTecnologia@outlook.com>
2025-02-25 17:53:08 -0300

    Person Entity

---
+++ api-client/src/main/java/br/com/hackthon/api_client/entities/Person.java
@@ -0,0 +1,59 @@
+package br.com.hackthon.api_client.entities;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+import java.util.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name = "tb_person")
+public class Person {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, length = 70)
+    private String firstName;
+
+    @Column(nullable = false, length = 170)
+    private String lastName;
+
+    @Column(nullable = false, name = "dateBirth")
+    private LocalDate dateBirth;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column
+    private Double income;
+
+    @Column(nullable = false)
+    private LocalDateTime collectionDate;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "family_id")
+    private Family family;
+
+    public Person(){
+
+    }
+
+    public Person(UUID id, String firstName, String lastName, LocalDate dateBirth, Integer age, String cpf, Double income, LocalDateTime collectionDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateBirth = dateBirth;
+        this.age = age;
+        this.cpf = cpf;
+        this.income = income;
+        this.collectionDate = collectionDate;
+    }
+}
