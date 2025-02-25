e7313085861f44d2bade5655cc983b3be649cd17
igorgcf <igorTecnologia@outlook.com>
2025-02-25 20:32:34 -0300

    Validator Error

--- api-client/src/main/java/br/com/hackthon/api_client/resources/exceptions/ValidatorError.java
+++ api-client/src/main/java/br/com/hackthon/api_client/resources/exceptions/ValidatorError.java
@@ -12,4 +12,19 @@ public class ValidatorError extends StandardError{
     public void addError(String fieldName, String message){
         errors.add(new FieldMessage(fieldName, message));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ValidatorError that = (ValidatorError) o;
+        return Objects.equals(errors, that.errors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), errors);
+    }
 }
+
