4344c465b761c33dca5c98db89a340959a237e4b
igorgcf <igorTecnologia@outlook.com>
2025-02-25 20:33:19 -0300

    Children DTO

--- api-client/src/main/java/br/com/hackthon/api_client/dto/ChildrenDTO.java
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/ChildrenDTO.java
@@ -83,4 +83,18 @@ public class ChildrenDTO extends RepresentationModel<ChildrenDTO> {
             }
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ChildrenDTO that = (ChildrenDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
