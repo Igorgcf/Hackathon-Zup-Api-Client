ec460e16fc96f6d31192b1f334b09d52d7eee97b
igorgcf <igorTecnologia@outlook.com>
2025-02-25 20:33:34 -0300

    User DTO

--- api-client/src/main/java/br/com/hackthon/api_client/dto/UserDTO.java
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/UserDTO.java
@@ -106,4 +106,18 @@ public class UserDTO extends RepresentationModel<UserDTO> {
         this(entity);
         roles.forEach(x -> this.roles.add(new RoleDTO(x)));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(id, userDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
